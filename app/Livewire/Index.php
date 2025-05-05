7f9a47d5ef9855a25262506fb9cfb5626eccec1b
Ferdilicius <1911592@alu.murciaeduca.es>
2025-05-05 20:48:02 +0200

    he quitado el bancal y he dejado el app en vez de el.

--- app/Livewire/Index.php
+++ app/Livewire/Index.php
@@ -8,6 +8,6 @@ class Index extends Component
 {
     public function render()
     {
-        return view('livewire.index')->layout('layouts.bancal.bancal');
+        return view('livewire.index')->layout('layouts.app');
     }
 }
