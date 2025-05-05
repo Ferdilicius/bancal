81e338b9ba1b710f4a0d23691f1650e213501ca3
Álvaro <alvaromartinez165penin@gmail.com>
2025-05-05 21:01:02 +0200

    Icono de fresa y arreglo de tablas mostrar-todos

--- resources/views/layouts/bancal/header.blade.php
+++ resources/views/layouts/bancal/header.blade.php
@@ -5,9 +5,9 @@
         <div class="container mx-auto flex justify-between items-center">
             <!-- Logo -->
             <a href="{{ url('/') }}">
-                <img src="{{ asset('images/la_fresa.png') }}" alt="Bancal" class="h-10 md:h-14">
+                <img src="{{ asset('img/iconFresa.svg') }}" alt="Bancal" class="h-24 md:h-28">
             </a>
-            
+
             <!-- User and Cart Icons -->
             <nav class="hidden md:flex space-x-4">
                 <a href="{{ route('profile.show') }}">
@@ -24,22 +24,28 @@
     <header x-data="{ open: false }" class="absolute top-0 left-0 w-full bg-transparent text-white p-4 md:hidden z-10">
         <div class="container mx-auto flex justify-between items-center">
             <!-- Logo -->
-            <a href="{{ url('/') }}">
-                <img src="{{ asset('images/la_fresa.png') }}" alt="Bancal" class="h-10">
-            </a>
+            <div>
+                <a href="{{ url('/') }}">
+                    <img src="{{ asset('img/iconFresa.svg') }}" alt="Bancal" class="h-24 w-24">
+                </a>
+            </div>
 
             <!-- Mobile Menu Button -->
-            <button @click="open = !open" class="focus:outline-none">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-                </svg>
-            </button>
-        </div>
-
-        <!-- Mobile Dropdown Menu -->
-        <div x-show="open" x-cloak @click.away="open = false" class="bg-gray-100 mt-2 p-4">
-            <a href="{{ route('profile.show') }}" class="block py-2 hover:bg-gray-200">Usuario</a>
-            <a href="" class="block py-2 hover:bg-gray-200">Carrito</a>
+            <div>
+                <button @click="open = !open" class="focus:outline-none">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M4 6h16M4 12h16m-7 6h7" />
+                    </svg>
+                </button>
+            </div>
+            <!-- Mobile Dropdown Menu -->
+            <div x-show="open" x-cloak @click.away="open = false"
+                class="bg-gray-300 bg-opacity-50 text-[#9E203F] p-4 absolute top-full left-0 w-full">
+                <a href="{{ route('profile.show') }}" class="block py-2 font-bold hover:bg-gray-200 hover:bg-opacity-50">Usuario</a>
+                <a href="" class="block py-2 font-bold hover:bg-gray-200 hover:bg-opacity-50">Carrito</a>
+            </div>
         </div>
     </header>
 </div>
