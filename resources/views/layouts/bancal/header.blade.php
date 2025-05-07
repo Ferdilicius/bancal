e1ef9cd9d91f1f2806b363b778b46313d71fd95b
Álvaro <alvaromartinez165penin@gmail.com>
2025-05-07 19:00:05 +0200

    FEAT: añadidos los iconos de en el header
    FIX: cambiado el header para que se vea mejor en dispositivos moviles

--- resources/views/layouts/bancal/header.blade.php
+++ resources/views/layouts/bancal/header.blade.php
@@ -5,16 +5,16 @@
         <div class="container mx-auto flex justify-between items-center">
             <!-- Logo -->
             <a href="{{ url('/') }}">
-                <img src="{{ asset('img/iconFresa.svg') }}" alt="Bancal" class="h-24 md:h-28">
+                <img src="{{ asset('img/iconFresa.svg') }}" alt="Bancal" class="h-20 md:h-28">
             </a>
 
             <!-- User and Cart Icons -->
             <nav class="hidden md:flex space-x-4">
-                <a href="{{ route('profile.show') }}">
-                    <img src="{{ asset('images/icono_usuario.png') }}" alt="Usuario" class="h-8">
+                <a href="{{ route('profile.show') }}" class="mr-4">
+                    <i class="fa-solid fa-user text-3xl"></i>
                 </a>
-                <a href="">
-                    <img src="{{ asset('images/icono_carrito.png') }}" alt="Carrito" class="h-8">
+                <a href="#" class="ml-4">
+                    <i class="fa-solid fa-cart-shopping text-3xl"></i>
                 </a>
             </nav>
         </div>
@@ -26,26 +26,19 @@
             <!-- Logo -->
             <div>
                 <a href="{{ url('/') }}">
-                    <img src="{{ asset('img/iconFresa.svg') }}" alt="Bancal" class="h-24 w-24">
+                    <img src="{{ asset('img/iconFresa.svg') }}" alt="Bancal" class="h-16 w-16">
                 </a>
             </div>
 
             <!-- Mobile Menu Button -->
-            <div>
-                <button @click="open = !open" class="focus:outline-none">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16m-7 6h7" />
-                    </svg>
-                </button>
-            </div>
-            <!-- Mobile Dropdown Menu -->
-            <div x-show="open" x-cloak @click.away="open = false"
-                class="bg-gray-300 bg-opacity-50 text-[#9E203F] p-4 absolute top-full left-0 w-full">
-                <a href="{{ route('profile.show') }}" class="block py-2 font-bold hover:bg-gray-200 hover:bg-opacity-50">Usuario</a>
-                <a href="" class="block py-2 font-bold hover:bg-gray-200 hover:bg-opacity-50">Carrito</a>
-            </div>
+            <nav class="flex space-x-4 md:hidden">
+                <a href="{{ route('profile.show') }}" class="mr-4">
+                    <i class="fa-solid fa-user text-2xl"></i>
+                </a>
+                <a href="#" class="ml-4">
+                    <i class="fa-solid fa-cart-shopping text-2xl"></i>
+                </a>
+            </nav>
         </div>
     </header>
 </div>
