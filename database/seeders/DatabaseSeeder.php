8752eb23191c9bd17d783adb1fde84c252b69ec3
Álvaro <alvaromartinez165penin@gmail.com>
2025-05-14 18:45:25 +0200

    Database Seeder arreglado

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,18 +16,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call(ProductSeeder::class);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
         $this->call(AddressTypeSeeder::class);
 
         $this->call(AddressSeeder::class);
-=======
->>>>>>> 6684643ade7598efa8bbdbf04b4ec01d8a30c251
-=======
-        // $this->call(AddressTypeSeeder::class);
-
-        // $this->call(AddressSeeder::class);
-
->>>>>>> 6cd1930dc1ab7a1af2cb8189e3c6cf5ce16b2de9
     }
 }
